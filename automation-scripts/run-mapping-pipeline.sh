7898621d48b1b40acdf2fa2e30d1c7422031dbaf
Mari Baker <118228251+mcbbaker@users.noreply.github.com>
2024-04-18 17:18:16 -0600

    Create run-mapping-pipeline.sh

---
+++ automation-scripts/run-mapping-pipeline.sh
@@ -0,0 +1,36 @@
+perl pipeline.pl pool51/config51 pool51-log
+mv *-best-sorted-rmdup.bam pool51
+mv *-best-sorted-rmdup.bam.csi pool51
+rm *.sam
+perl pipeline.pl pool52/config52 pool52-log
+mv *-best-sorted-rmdup.bam pool52
+mv *-best-sorted-rmdup.bam.csi pool52
+rm *.sam
+perl pipeline.pl pool53/config53 pool53-log
+mv *-best-sorted-rmdup.bam pool53
+mv *-best-sorted-rmdup.bam.csi pool53
+rm *.sam
+perl pipeline.pl pool54/config54 pool54-log
+mv *-best-sorted-rmdup.bam pool54
+mv *-best-sorted-rmdup.bam.csi pool54
+rm *.sam
+perl pipeline.pl pool55/config55 pool55-log
+mv *-best-sorted-rmdup.bam pool55
+mv *-best-sorted-rmdup.bam.csi pool55
+rm *.sam
+perl pipeline.pl pool56/config56 pool56-log
+mv *-best-sorted-rmdup.bam pool56
+mv *-best-sorted-rmdup.bam.csi pool56
+rm *.sam
+perl pipeline.pl pool57/config57 pool57-log
+mv *-best-sorted-rmdup.bam pool57
+mv *-best-sorted-rmdup.bam.csi pool57
+rm *.sam
+perl pipeline.pl pool58/config58 pool58-log
+mv *-best-sorted-rmdup.bam pool58
+mv *-best-sorted-rmdup.bam.csi pool58
+rm *.sam
+perl pipeline.pl pool59/config59 pool59-log
+mv *-best-sorted-rmdup.bam pool59
+mv *-best-sorted-rmdup.bam.csi pool59
+rm *.sam
